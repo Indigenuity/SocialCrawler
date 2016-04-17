263084aa2fa29ad0c2ed5ce800191da1c7b4e973
Indigenuity <indigenuity@users.noreply.github.com>
2016-04-16 20:25:30 -0600

    First experiments in fetching and storing FBPages

--- app/experiment/Experiment.java
+++ app/experiment/Experiment.java
@@ -1,5 +1,6 @@
 package experiment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.restfb.Connection;
@@ -10,12 +11,14 @@ import com.restfb.scope.ScopeBuilder;
 import com.restfb.scope.UserDataPermissions;
 import com.restfb.Parameter;
 import com.restfb.Version;
+import com.restfb.types.Category;
 import com.restfb.types.Comment;
 import com.restfb.types.Comments;
 import com.restfb.types.Page;
 import com.restfb.types.Post;
 import com.restfb.types.User;
 
+import persistence.FBPage;
 import persistence.Test;
 import play.db.jpa.JPA;
 
@@ -26,11 +29,103 @@ public class Experiment {
 	private static final String REDIRECT = "https://www.facebook.com/connect/login_success.html";
 	private static final String TEMP_USER_ACCESS = "AQBhzmEw3hmKQ_ffKvyVxI1GIdTop6-epXaeGIwRQzkR54VTfSJkJ8cNBwWn_jKqAbIi00bSQGyaGoC84dT4Nl2DXM6B04X6Ypt_ifeuenuf0UPVse4lqbFBKD86ZQ6dmvnL_T3Pvf4iTL9OytEJGQklKhR3lkv28EdYpdhJOBHszq4FM8qc28BRrjCUpnO-i3dk6kZyFVt9-k1kHsmxg-g1blfYWOmZShlCKvXYk-bOqbqJiu6D0Q_qroof8_yStP1IWa5HFnIswBKdhC6FmimmvrCGxGNUlv6qRVczTnhIkaUBN5vrh4vCmeUF1Cr2VcY";
 
-	public static void runExperiment() {
-		Test test = new Test();
-		JPA.em().persist(test);
-		System.out.println("id : " + test.testId);
+	public static void runExperiment(){
+		fetchPage();
 	}
+	
+	public static void fetchPage() {
+		AccessToken accessToken = new DefaultFacebookClient().obtainAppAccessToken(APP_ID, APP_SECRET);
+		FacebookClient facebookClient = new DefaultFacebookClient(accessToken.getAccessToken(), Version.LATEST);
+		Page page = facebookClient.fetchObject("PizzaPieCafe",  Page.class, Parameter.with("summary",true), 
+				Parameter.with("fields", "about,affiliation,app_id,attire,best_page,built,can_checkin,category,category_list,"
+						+ "checkins,company_overview,contact_address,culinary_team,current_location,description,display_subtext,"
+						+ "emails,fan_count,features,food_styles,founded,general_info,general_manager,global_brand_root_id,"
+						+ "hours,impressum, is_always_open,is_community_page,is_permanently_closed,is_unclaimed,is_verified,link,"
+						+ "location,members,mission,mpg,name, parent_page,parking,payment_options, personal_info, personal_interests,"
+						+ "pharma_safety_info,phone,place_type,price_range, products,public_transit,restaurant_services,"
+						+ "restaurant_specialties,single_line_address,start_info,store_location_descriptor,store_number,"
+						+ "talking_about_count,username,verification_status,voip_info,website,were_here_count"));
+		FBPage fbPage = new FBPage();
+		fbPage.setGivenUrl("kengarffauto");
+		fbPage.setAbout(page.getAbout());
+		fbPage.setAffiliation(page.getAffiliation());
+		fbPage.setAppId(page.getAppId());
+		fbPage.setAttire(page.getAttire());
+		if(page.getBestPage() != null) {
+			fbPage.setBestPage(page.getBestPage().getId());
+		}
+		fbPage.setBuilt(page.getBuilt()); 
+		fbPage.setCanCheckin(page.getCanCheckin());
+		fbPage.setCategory(page.getCategory());
+		if(page.getCategoryList() != null){
+			List<String> categories = new ArrayList<String>();
+			for(Category category : page.getCategoryList()){
+				categories.add(category.getName());
+			}
+			fbPage.setSubCategories(categories);
+		}
+		fbPage.setCheckins(page.getCheckins());
+		fbPage.setOverview(page.getCompanyOverview());
+		fbPage.setContactAddress(page.getContactAddress() + "");
+		fbPage.setCulinaryTeam(page.getCulinaryTeam());
+		fbPage.setCurrentLocation(page.getCurrentLocation());
+		fbPage.setDescription(page.getDescription());
+		fbPage.setDisplaySubtext(page.getDisplaySubtext());
+		if(page.getEmails() != null){
+			fbPage.setEmails(page.getEmails());
+		}
+		fbPage.setLikes(page.getFanCount());
+		fbPage.setFeatures(page.getFeatures());
+		if(page.getFoodStyles() != null){
+			fbPage.setFoodStyles(page.getFoodStyles());
+		}
+		fbPage.setFounded(page.getFounded());
+		fbPage.setGeneralInfo(page.getGeneralInfo());
+		fbPage.setGeneralManager(page.getGeneralManager());
+		fbPage.setGlobalBrandRootId(page.getGlobalBrandRootId());
+		fbPage.setHours(page.getHours() + "");
+		fbPage.setImpressum(page.getImpressum());
+		fbPage.setAlwaysOpen(page.getIsAlwaysOpen());
+		fbPage.setCommunityPage(page.getIsCommunityPage());
+		fbPage.setPermanentlyClosed(page.getIsPermanentlyClosed());
+		fbPage.setUnclaimed(page.getIsUnclaimed());
+		fbPage.setVerified(page.getIsVerified());
+		fbPage.setLink(page.getLink());
+		fbPage.setLocation(page.getLocation() + "");
+		fbPage.setMembers(page.getMembers());
+		fbPage.setMission(page.getMission());
+		fbPage.setMpg(page.getMpg());
+		fbPage.setName(page.getName());
+		fbPage.setPaymentOptions(page.getPaymentOptions() + "");
+		fbPage.setPersonalInfo(page.getPersonalInfo());
+		fbPage.setPersonalInterests(page.getPersonalInterests());
+		fbPage.setPharmaSafetyInfo(page.getPharmaSafetyInfo());
+		fbPage.setPhone(page.getPhone());
+		fbPage.setPlaceType(page.getPlaceType());
+		fbPage.setPriceRange(page.getPriceRange());
+		fbPage.setProducts(page.getProducts());
+		fbPage.setPublicTransit(page.getPublicTransit());
+		fbPage.setRestaurantServices(page.getRestaurantServices() + "");
+		fbPage.setRestaurantSpecialties(page.getRestaurantSpecialties() + "");
+		fbPage.setSingleLineAddress(page.getSingleLineAddress());
+		if(page.getStartInfo() != null){
+			fbPage.setStartDate(page.getStartInfo().getDate() + "");
+			fbPage.setStartType(page.getStartInfo().getType()); 
+		}
+		fbPage.setStoreLocationDescriptor(page.getNameWithLocationDescriptor());
+		fbPage.setStoreNumber(page.getStoreNumber());
+		fbPage.setTalkingAbout(page.getTalkingAboutCount());
+		fbPage.setUsername(page.getUsername());
+		fbPage.setVerificationStatus(page.getVerificationStatus());
+		fbPage.setVoipInfo(page.getVoipInfo() + "");
+		fbPage.setWebsite(page.getWebsite());
+		fbPage.setWereHere(page.getWereHereCount());
+		
+		JPA.em().persist(fbPage);
+		System.out.println("page : " + page.getHours());
+		System.out.println("page likes : "  + page.getDisplaySubtext());
+	}
+	
 	
 	public static void fbClientTesting() throws Exception {
 		try{
--- app/persistence/FBPage.java
+++ app/persistence/FBPage.java
@@ -1,10 +1,12 @@
 package persistence;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 
 import org.apache.commons.lang3.StringUtils;
 
+@Entity
 public class FBPage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,39 +23,67 @@ public class FBPage {
 	@Column(unique=true)
 	private String givenUrl;
 	private String fbId;
-	@Column(nullable = true, columnDefinition="varchar(2000)")
 	private String about;
+	private String affiliation;
+	private String appId;
+	private String attire;
+	private String bestPage;
+	private String built;
+	private Boolean canCheckin;
 	private String category;
-	@Column(nullable = true, columnDefinition="varchar(2000)")
+	private Integer checkins;
 	private String overview;
 	private String contactAddress;
+	private String culinaryTeam;
 	private String currentLocation;
-	@Column(nullable = true, columnDefinition="varchar(2000)")
 	private String description;
+	private String displaySubtext;
+	private String features;
 	private String founded;
-	@Column(nullable = true, columnDefinition="varchar(2000)")
 	private String generalInfo;
 	private String generalManager;
+	private String globalBrandRootId;
+	private String hours;
+	private String impressum;
 	private String link; 
-	@Column(nullable = true, columnDefinition="varchar(2000)")
+	private String location;
+	private String members;
 	private String mission;
+	private String mpg;
 	private String name;
-	private String nameWithBrand;
+	private String nameWithBrandOrLocation;
+	private String parking;
+	private String paymentOptions;
 	private String parentPageId;
 	private String personalInfo;
 	private String personalInterests;
+	private String pharmaSafetyInfo;
 	private String phone;
+	private String placeType;
+	private String priceRange;
 	private String products;
+	private String publicTransit;
+	private String restaurantServices;
+	private String restaurantSpecialties;
+	private String storeLocationDescriptor;
 	private String storeNumber;
 	private String singleLineAddress;
+	private String startDate;
+	private String startType;
+	private Long talkingAbout;
 	private String username;
+	private String verificationStatus;
+	private String voipInfo;
+	private Long wereHere;
 	
 	@Column(nullable = true, columnDefinition="varchar(4000)")
 	private String website;
-	private boolean permanentlyClosed;
-	private boolean unclaimed;
-	private boolean verified;
-	private long likes;
+	private Boolean permanentlyClosed;
+	private Boolean verified;
+	private Boolean unclaimed;
+	private Boolean alwaysOpen;
+	private Boolean communityPage;
+	private Long likes;
 	
 	@ElementCollection
 	@Column(columnDefinition="varchar(255)")
@@ -60,11 +91,13 @@ public class FBPage {
 	@ElementCollection(fetch=FetchType.EAGER)
 	@Column(columnDefinition="varchar(255)")
 	private List<String> emails = new ArrayList<String>();
-	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@Column(columnDefinition="varchar(255)")
+	private List<String> foodStyles = new ArrayList<String>();
 	
 	private String errorCode;
 	private String errorMessage;
-	
+//	
 	private long getFbPageId() {
 		return fbPageId;
 	}
@@ -101,55 +134,55 @@ public class FBPage {
 		return about;
 	}
 	public void setAbout(String about) {
-		this.about = StringUtils.abbreviate(about, 2000);
+		this.about = StringUtils.abbreviate(about, 255);
 	}
 	public String getCategory() {
 		return category;
 	}
 	public void setCategory(String category) {
-		this.category = category;
+		this.category = StringUtils.abbreviate(category, 255);
 	}
 	public String getOverview() {
 		return overview;
 	}
 	public void setOverview(String overview) {
-		this.overview =StringUtils.abbreviate(overview, 2000); 
+		this.overview =StringUtils.abbreviate(overview, 255); 
 	}
 	public String getContactAddress() {
 		return contactAddress;
 	}
 	public void setContactAddress(String contactAddress) {
-		this.contactAddress = contactAddress;
+		this.contactAddress = StringUtils.abbreviate(contactAddress, 255);
 	}
 	public String getCurrentLocation() {
 		return currentLocation;
 	}
 	public void setCurrentLocation(String currentLocation) {
-		this.currentLocation = currentLocation;
+		this.currentLocation = StringUtils.abbreviate(currentLocation, 255);
 	}
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
-		this.description =StringUtils.abbreviate(description, 2000); 
+		this.description =StringUtils.abbreviate(description, 255); 
 	}
 	public String getFounded() {
 		return founded;
 	}
 	public void setFounded(String founded) {
-		this.founded = founded;
+		this.founded = StringUtils.abbreviate(founded, 255);
 	}
 	public String getGeneralInfo() {
 		return generalInfo;
 	}
 	public void setGeneralInfo(String generalInfo) {
-		this.generalInfo = StringUtils.abbreviate(generalInfo, 2000);
+		this.generalInfo = StringUtils.abbreviate(generalInfo, 255);
 	}
 	public String getGeneralManager() {
 		return generalManager;
 	}
 	public void setGeneralManager(String generalManager) {
-		this.generalManager = generalManager;
+		this.generalManager =StringUtils.abbreviate( generalManager, 255);
 	}
 	public String getLink() {
 		return link;
@@ -161,25 +194,19 @@ public class FBPage {
 		return mission;
 	}
 	public void setMission(String mission) {
-		this.mission = StringUtils.abbreviate(mission, 2000);
+		this.mission = StringUtils.abbreviate(mission, 255);
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
-		this.name = name;
-	}
-	public String getNameWithBrand() {
-		return nameWithBrand;
-	}
-	public void setNameWithBrand(String nameWithBrand) {
-		this.nameWithBrand = nameWithBrand;
+		this.name = StringUtils.abbreviate(name, 255);
 	}
 	public String getParentPageId() {
 		return parentPageId;
 	}
 	public void setParentPageId(String parentPageId) {
-		this.parentPageId = parentPageId;
+		this.parentPageId = StringUtils.abbreviate(parentPageId, 255);
 	}
 	public String getPersonalInfo() {
 		return personalInfo;
@@ -197,25 +224,25 @@ public class FBPage {
 		return phone;
 	}
 	public void setPhone(String phone) {
-		this.phone = phone;
+		this.phone = StringUtils.abbreviate(phone, 255);
 	}
 	public String getProducts() {
 		return products;
 	}
 	public void setProducts(String products) {
-		this.products = products;
+		this.products = StringUtils.abbreviate(products, 255);
 	}
 	public String getStoreNumber() {
 		return storeNumber;
 	}
 	public void setStoreNumber(String storeNumber) {
-		this.storeNumber = storeNumber;
+		this.storeNumber = StringUtils.abbreviate(storeNumber, 255);
 	}
 	public String getSingleLineAddress() {
 		return singleLineAddress;
 	}
 	public void setSingleLineAddress(String singleLineAddress) {
-		this.singleLineAddress = singleLineAddress;
+		this.singleLineAddress = StringUtils.abbreviate(singleLineAddress, 255);
 	}
 	public String getUsername() {
 		return username;
@@ -257,13 +284,224 @@ public class FBPage {
 		return errorCode;
 	}
 	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
+		this.errorCode = StringUtils.abbreviate(errorCode, 255);
 	}
 	public String getErrorMessage() {
 		return errorMessage;
 	}
 	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
+		this.errorMessage = StringUtils.abbreviate(errorMessage, 255);
+	}
+	public String getAffiliation() {
+		return affiliation;
+	}
+	public void setAffiliation(String affiliation) {
+		this.affiliation = StringUtils.abbreviate(affiliation, 255);
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = StringUtils.abbreviate(appId, 255);
+	}
+	public String getAttire() {
+		return attire;
+	}
+	public void setAttire(String attire) {
+		this.attire = StringUtils.abbreviate(attire, 255);
+	}
+	public String getBestPage() {
+		return bestPage;
+	}
+	public void setBestPage(String bestPage) {
+		this.bestPage =StringUtils.abbreviate( bestPage, 255);
+	}
+	public String getBuilt() {
+		return built;
+	}
+	public void setBuilt(String built) {
+		this.built = StringUtils.abbreviate(built, 255);
+	}
+	public boolean isCanCheckin() {
+		return canCheckin;
+	}
+	public void setCanCheckin(boolean canCheckin) {
+		this.canCheckin = canCheckin;
+	}
+	public String getCulinaryTeam() {
+		return culinaryTeam;
+	}
+	public void setCulinaryTeam(String culinaryTeam) {
+		this.culinaryTeam = StringUtils.abbreviate(culinaryTeam, 255);
+	}
+	public String getDisplaySubtext() {
+		return displaySubtext;
+	}
+	public void setDisplaySubtext(String displaySubtext) {
+		this.displaySubtext = StringUtils.abbreviate(displaySubtext, 255);
+	}
+	public List<String> getFoodStyles() {
+		return foodStyles;
+	}
+	public void setFoodStyles(List<String> foodStyles) {
+		this.foodStyles.clear();
+		this.foodStyles.addAll(foodStyles);
+	}
+	public int getCheckins() {
+		return checkins;
+	}
+	public void setCheckins(int checkins) {
+		this.checkins = checkins;
+	}
+	public String getFeatures() {
+		return features;
+	}
+	public void setFeatures(String features) {
+		this.features = StringUtils.abbreviate(features, 255);
+	}
+	public String getGlobalBrandRootId() {
+		return globalBrandRootId;
+	}
+	public void setGlobalBrandRootId(String globalBrandRootId) {
+		this.globalBrandRootId = StringUtils.abbreviate(globalBrandRootId, 255);
+	}
+	public String getHours() {
+		return hours;
+	}
+	public void setHours(String hours) {
+		this.hours = StringUtils.abbreviate(hours, 255);
+	}
+	public String getImpressum() {
+		return impressum;
+	}
+	public void setImpressum(String impressum) {
+		this.impressum = StringUtils.abbreviate(impressum, 255);
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = StringUtils.abbreviate(location, 255);
+	}
+	public String getMembers() {
+		return members;
+	}
+	public void setMembers(String members) {
+		this.members = StringUtils.abbreviate(members, 255);
+	}
+	public String getMpg() {
+		return mpg;
+	}
+	public void setMpg(String mpg) {
+		this.mpg =StringUtils.abbreviate( mpg, 255);
+	}
+	public String getNameWithBrandOrLocation() {
+		return nameWithBrandOrLocation;
+	}
+	public void setNameWithBrandOrLocation(String nameWithBrandOrLocation) {
+		this.nameWithBrandOrLocation = StringUtils.abbreviate(nameWithBrandOrLocation, 255);
+	}
+	public String getParking() {
+		return parking;
+	}
+	public void setParking(String parking) {
+		this.parking = StringUtils.abbreviate(parking, 255);
+	}
+	public String getPaymentOptions() {
+		return paymentOptions;
+	}
+	public void setPaymentOptions(String paymentOptions) {
+		this.paymentOptions = StringUtils.abbreviate(paymentOptions, 255);
+	}
+	public String getPharmaSafetyInfo() {
+		return pharmaSafetyInfo;
+	}
+	public void setPharmaSafetyInfo(String pharmaSafetyInfo) {
+		this.pharmaSafetyInfo = StringUtils.abbreviate(pharmaSafetyInfo, 255);
+	}
+	public String getPlaceType() {
+		return placeType;
+	}
+	public void setPlaceType(String placeType) {
+		this.placeType = StringUtils.abbreviate(placeType, 255);
+	}
+	public String getPriceRange() {
+		return priceRange;
+	}
+	public void setPriceRange(String priceRange) {
+		this.priceRange = StringUtils.abbreviate(priceRange, 255);
+	}
+	public String getPublicTransit() {
+		return publicTransit;
+	}
+	public void setPublicTransit(String publicTransit) {
+		this.publicTransit = StringUtils.abbreviate(publicTransit, 255);
+	}
+	public String getRestaurantServices() {
+		return restaurantServices;
+	}
+	public void setRestaurantServices(String restaurantServices) {
+		this.restaurantServices = StringUtils.abbreviate(restaurantServices, 255);
+	}
+	public String getRestaurantSpecialties() {
+		return restaurantSpecialties;
+	}
+	public void setRestaurantSpecialties(String restaurantSpecialties) {
+		this.restaurantSpecialties =StringUtils.abbreviate( restaurantSpecialties, 255);
+	}
+	public String getStoreLocationDescriptor() {
+		return storeLocationDescriptor;
+	}
+	public void setStoreLocationDescriptor(String storeLocationDescriptor) {
+		this.storeLocationDescriptor =StringUtils.abbreviate( storeLocationDescriptor, 255);
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = StringUtils.abbreviate(startDate, 255);
+	}
+	public String getStartType() {
+		return startType;
+	}
+	public void setStartType(String startType) {
+		this.startType = StringUtils.abbreviate(startType, 255);
+	}
+	public Long getTalkingAbout() {
+		return talkingAbout;
+	}
+	public void setTalkingAbout(Long talkingAbout) {
+		this.talkingAbout = talkingAbout;
+	}
+	public String getVerificationStatus() {
+		return verificationStatus;
+	}
+	public void setVerificationStatus(String verificationStatus) {
+		this.verificationStatus = StringUtils.abbreviate(verificationStatus, 255);
+	}
+	public String getVoipInfo() {
+		return voipInfo;
+	}
+	public void setVoipInfo(String voipInfo) {
+		this.voipInfo = StringUtils.abbreviate(voipInfo, 255);
+	}
+	public Long getWereHere() {
+		return wereHere;
+	}
+	public void setWereHere(Long wereHere) {
+		this.wereHere = wereHere;
+	}
+	public boolean isAlwaysOpen() {
+		return alwaysOpen;
+	}
+	public void setAlwaysOpen(boolean alwaysOpen) {
+		this.alwaysOpen = alwaysOpen;
+	}
+	public boolean isCommunityPage() {
+		return communityPage;
+	}
+	public void setCommunityPage(boolean communityPage) {
+		this.communityPage = communityPage;
 	}
 	
 	
